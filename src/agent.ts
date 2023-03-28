ecc1178032bbde385ca21ed8b9810e0cdf0af770
xhola <ohksamie@gmail.com>
2023-03-28 01:24:08 +0100

    tweaks to enable agent run

--- src/agent.ts
+++ src/agent.ts
@@ -1,7 +1,7 @@
 import { EntityType, Finding, FindingSeverity, FindingType, HandleAlert, AlertEvent, Initialize } from 'forta-agent'
 import { BOT_ID } from './constants'
 
-const ALERT_THRESHOLD = 2; 
+const ALERT_THRESHOLD = 2;
 
 const initialize: Initialize = async () => {
   return {
@@ -17,10 +17,10 @@ const initialize: Initialize = async () => {
 }
 
 const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-  const findings: Finding[] = []
+  let findings: Finding[] = [];
   const alert = alertEvent.alert
   const alertId = alert?.alertId;
-  const creatorAddress = alert.labels?.find(label => label.entityType === EntityType.Address)?.entity ?? '';
+  const contractAddress = alert?.contracts?.[0]?.address;
   const uniqueAlertIds: Set<string> = new Set();
   uniqueAlertIds.add(alertId || '')
 
@@ -28,14 +28,14 @@ const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
     const findings = [
       Finding.fromObject({
         name: "Multiple Soft Rug Pulls Detected",
-        description: `Multiple soft rug pulls have been detected for address ${creatorAddress}.`,
+        description: `Multiple soft rug pulls have been detected for address ${contractAddress}.`,
         alertId: "SOFT-RUG-PULL",
         severity: FindingSeverity.High,
         type: FindingType.Suspicious,
         labels: [
           {
             entityType: EntityType.Address,
-            entity: creatorAddress,
+            entity: contractAddress || '',
             label: "soft-rug-pull-scam",
             confidence: 1,
             remove: false,
