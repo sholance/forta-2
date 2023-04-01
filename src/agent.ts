aa5e913cfe680f863b45106545a5420a89639d6e
xhola <ohksamie@gmail.com>
2023-04-01 13:57:11 +0100

    update botIds and error logging

--- src/agent.ts
+++ src/agent.ts
@@ -9,45 +9,60 @@ const initialize: Initialize = async () => {
       subscriptions: [
         {
           botId: BOT_ID,
-          alertIds: ["RUG-1", "RUG-2", "RUG-3"],
+          alertIds: ["SOFT-RUG-PULL-SUS-LIQ-POOL-CREATION", "SOFT-RUG-PULL-SUS-LIQ-POOL-RESERVE-CHANGE", "SOFT-RUG-PULL-SUS-POOL-REMOVAL"],
         },
       ],
     },
   }
 }
 
-const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-  let findings: Finding[] = [];
-  const alert = alertEvent.alert
-  const alertId = alert?.alertId;
-  const contractAddress = alert?.contracts?.[0]?.address;
-  const uniqueAlertIds: Set<string> = new Set();
-  uniqueAlertIds.add(alertId || '')
+const handleAlert: HandleAlert = async (alertEvent: AlertEvent): Promise<Finding[]> => {
+  try {
+    let findings: Finding[] = [];
+    const alert = alertEvent.alert
+    const alertId = alert?.alertId;
+    const contractAddress = alert?.contracts?.[0]?.address;
+    const attacker = alert?.contracts?.[0]?.address?.[0];
+    const uniqueAlertIds: Set<string> = new Set();
+    uniqueAlertIds.add(alertId || '')
 
-  if (uniqueAlertIds.size > ALERT_THRESHOLD) {
-    const findings = [
-      Finding.fromObject({
-        name: "Multiple Soft Rug Pulls Detected",
-        description: `Multiple soft rug pulls have been detected for address ${contractAddress}.`,
-        alertId: "SOFT-RUG-PULL",
-        severity: FindingSeverity.High,
-        type: FindingType.Suspicious,
-        labels: [
-          {
-            entityType: EntityType.Address,
-            entity: contractAddress || '',
-            label: "soft-rug-pull-scam",
-            confidence: 1,
-            remove: false,
-            metadata: {},
-          },
-        ],
-      }),
-    ]
+    if (uniqueAlertIds.size > ALERT_THRESHOLD) {
+      const findings = [
+        Finding.fromObject({
+          name: "Multiple Soft Rug Pulls Detected",
+          description: `Multiple soft rug pulls have been detected for address ${contractAddress}.`,
+          alertId: "SOFT-RUG-PULL",
+          severity: FindingSeverity.High,
+          type: FindingType.Suspicious,
+          labels: [
+            {
+              entityType: EntityType.Address,
+              entity: contractAddress || '',
+              label: "soft-rug-pull-scam",
+              confidence: 1,
+              remove: false,
+              metadata: {},
+            },
+            {
+              entityType: EntityType.Address,
+              entity: attacker || '',
+              label: "attacker",
+              confidence: 1,
+              remove: false,
+              metadata: {},
+            },
+          ],
+        }),
+      ]
+      return findings
+    }
     return findings
+  } catch (error) {
+    console.log(error)
+    return []
   }
-  return findings
 }
+
 
 export default {
   initialize,
