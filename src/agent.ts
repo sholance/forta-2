a934d7a3d17c6b41ab4d3f4c7fd5d88e0a2171a2
Victor Samuel <54833975+sholance@users.noreply.github.com>
2023-04-25 18:11:39 +0000

    update finding methods

--- src/agent.ts
+++ src/agent.ts
@@ -1,8 +1,8 @@
 import { EntityType, Finding, FindingSeverity, FindingType, HandleAlert, AlertEvent, Initialize, getEthersProvider } from 'forta-agent'
 import { BOT_ID } from './constants'
 
-const ALERT_THRESHOLD = 1;
-const alertDict: { [key: string]: { alerts: Set<any>, alertIds: Set<string>, alertHashes: Set<string>, txHashes: Set<string> } } = {}
+const ALERT_THRESHOLD = 2;
+const alertDict: Record<string, { alerts: Set<AlertEvent>, alertIds: Set<string>, alertHashes: Set<string>, txHashes: Set<string> }> = {}
 
 const initialize: Initialize = async () => {
   const { chainId } = await getEthersProvider().getNetwork();
@@ -13,114 +13,109 @@ const initialize: Initialize = async () => {
         {
           botId: BOT_ID,
           alertIds: ["SOFT-RUG-PULL-SUS-LIQ-POOL-CREATION", "SOFT-RUG-PULL-SUS-LIQ-POOL-RESERVE-CHANGE", "SOFT-RUG-PULL-SUS-POOL-REMOVAL"],
-          chainId: chainId
-        }
+          chainId,
+        },
       ],
     },
   }
 }
 
 const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-  let findings: Finding[] = [];
-  const alert = alertEvent.alert
-  const alertId = alert?.alertId;
-  const alertHash = alert?.hash;
-  const txHash = alert.metadata.transaction
+  const findings: Finding[] = [];
+  const alert = alertEvent.alert;
+  const { alertId, hash } = alert ?? {};
+  const txHash = alert.metadata.transaction;
   const alertType = alert?.findingType;
   const address = alert.metadata?.tokenAddress;
 
+  if (alertId && hash && address) {
+    if (!alertDict[address]) {
+      alertDict[address] = { alerts: new Set(), alertIds: new Set(), alertHashes: new Set(), txHashes: new Set() };
+    }
+    alertDict[address].alertIds.add(alertId);
+    alertDict[address].alertHashes.add(hash);
+    alertDict[address].alerts.add(alertEvent);
+    alertDict[address].txHashes.add(txHash);
 
-  if (alertId && alertHash && address) {
-      if (!alertDict[address]) {
-        alertDict[address] = { alerts: new Set (), alertIds: new Set(), alertHashes: new Set(), txHashes: new Set() };
-      }
-      alertDict[address].alertIds.add(alertId);
-      alertDict[address].alertHashes.add(alertHash);
-      alertDict[address].alerts.add(alert)
-      alertDict[address].txHashes.add(txHash)
-
-
-    if (alertDict[address]?.alertIds.size >= ALERT_THRESHOLD) {
+    if (alertDict[address].alertIds.size >= ALERT_THRESHOLD) {
       try {
-        for (const [address, { alertIds, alertHashes, txHashes }] of Object.entries(alertDict)) {
-          const alerts = Array.from(alertDict[address].alerts)
+        const alerts = Array.from(alertDict[address].alerts);
+        const findingsCount = alertDict[address].alertIds.size;
+        if (alerts.length > 1 && alerts.every((a) => a.alert.metadata.tokenAddress === alerts[0].alert.metadata.tokenAddress) && findingsCount >= ALERT_THRESHOLD) {
+          const alertIdString = Array.from(alertDict[address].alertIds).join(" && ");
+          const alertHashString = Array.from(alertDict[address].alertHashes).join(" && ");
+          const txHashString = Array.from(alertDict[address].txHashes).join(" && ");
 
-          const findingsCount = alertIds.size;
-          if (alerts.length > ALERT_THRESHOLD) {
-            const alertIdString = Array.from(alertIds).join(" && ");
-            const alertHashString = Array.from(alertHashes).join(" && ");
-            const txHashString = Array.from(txHashes).join(" && ")
-
-            const finding = Finding.fromObject({
-              name: "Soft Rug Pulls Detected",
-              description: `Likely Soft rug pull has been detected`,
-              alertId: alertIdString,
-              severity: FindingSeverity.High,
-              type: FindingType.Suspicious,
-              labels: [
-                {
-                  entityType: EntityType.Address,
-                  entity: address,
-                  label: "soft-rug-pull-scam",
-                  confidence: 1,
-                  remove: false,
-                  metadata: {},
-                },
-                {
-                  entityType: EntityType.Address,
-                  entity: address,
-                  label: "attacker",
-                  confidence: 1,
-                  remove: false,
-                  metadata: {},
-                },
-              ],
-              metadata: {
-                alert_hash: alertHashString,
-                bot_id: alert.source?.bot?.id!,
-                alert_id: alertIdString,
-                contractAddress: alert.metadata.contractAddress,
-                token: alert.metadata?.tokenAddress,
-                deployer: alert.metadata?.deployer,
-                txHashes: txHashString,
+          const finding = Finding.fromObject({
+            name: "Soft Rug Pulls Detected",
+            description: `Likely Soft rug pull has been detected`,
+            alertId: alertIdString,
+            severity: FindingSeverity.High,
+            type: FindingType.Suspicious,
+            labels: [
+              {
+                entityType: EntityType.Address,
+                entity: address,
+                label: "soft-rug-pull-scam",
+                confidence: 1,
+                remove: false,
+                metadata: {},
               },
-            });
-            findings.push(finding);
-          }
+              {
+                entityType: EntityType.Address,
+                entity: address,
+                label: "attacker",
+                confidence: 1,
+                remove: false,
+                metadata: {},
+              },
+            ],
+            metadata: {
+              alert_hash: alertHashString,
+              bot_id: alert?.source?.bot?.id!,
+              alert_id: alertIdString,
+              contractAddress: alert.metadata.contractAddress,
+              token: alerts[0].alert.metadata.tokenAddress,
+              deployer: alert.metadata.deployer,
+              txHashes: txHashString,
+            },
+          });
+          findings.push(finding);
         }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    if (alertType === 'Exploit') {
-      const finding = Finding.fromObject({
-        name: "Soft Rug Pulls",
-        description: `Soft rug pull has been detected`,
-        alertId: alertId!,
-        severity: FindingSeverity.High,
-        type: FindingType.Suspicious,
-        labels: [
-          {
-            entityType: EntityType.Address,
-            entity: address,
-            label: "potential-exploit",
-            confidence: 1,
-            remove: false,
-            metadata: {},
-          },
-        ],
-        metadata: {
-          alert_hash: alert.hash!,
-          bot_id: alert.source?.bot?.id!,
-          contractAddress: alert.metadata.contractAddress,
-          token: alert.metadata?.tokenAddress,
-          deployer: alert.metadata.deployer,
+      } catch (error)
+{
+  console.log(error)
+}
+  }
+  if (alert && alertType === 'Exploit') {
+    const finding = Finding.fromObject({
+      name: "Soft Rug Pulls",
+      description: `Soft rug pull has been detected`,
+      alertId: alertId!,
+      severity: FindingSeverity.High,
+      type: FindingType.Suspicious,
+      labels: [
+        {
+          entityType: EntityType.Address,
+          entity: address,
+          label: "potential-exploit",
+          confidence: 1,
+          remove: false,
+          metadata: {},
         },
-      })
-      findings.push(finding);
-    }
+      ],
+      metadata: {
+        alert_hash: alert.hash!,
+        bot_id: alert.source?.bot?.id!,
+        contractAddress: alert.metadata.contractAddress,
+        token: alert.metadata?.tokenAddress,
+        deployer: alert.metadata.deployer,
+      },
+    })
+    findings.push(finding);
   }
 
+  }
   return findings
 
 }
